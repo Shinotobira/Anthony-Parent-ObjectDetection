95e0a43e7ddf8e944e20ef15e26d205e29e5a0a2
Shinotobira <62753305+Shinotobira@users.noreply.github.com>
2025-01-14 09:49:12 +0100

    feat: add github pipeline

--- Anthony.Parent.ObjectDetection/ObjectDetection.cs
+++ Anthony.Parent.ObjectDetection/ObjectDetection.cs
@@ -5,34 +5,50 @@ namespace Anthony.Parent.ObjectDetection;
 public class ObjectDetection
 {
 
-   /* public async Task<IList<ObjectDetectionResult>>
+    public async Task<IList<ObjectDetectionResult>>
         DetectObjectInScenesAsync(IList<byte[]> imagesSceneData)
     {
         await Task.Delay(1000);
-// TODO implement your code here
-        throw new NotImplementedException();
-    }*/
-    
-    public async Task<IList<ObjectDetectionResult>> DetectObjectInScenesAsync(IList<byte[]>
-        imagesSceneData)
-    {
-        IList<ObjectDetectionResult> results = new List<ObjectDetectionResult>();
-        results.Add(new ObjectDetectionResult
-        { ImageData = [0],
-            Box = new List<BoundingBox>()
-            {
-                new() {Confidence=0.5f,Label="Car", Dimensions = new
-                    BoundingBoxDimensions() { Height = 2, Width = 2, Y = 0, X= 0} },
-            }} );
-        results.Add(new ObjectDetectionResult
-        { ImageData = [0],
-            Box = new List<BoundingBox>()
-            {
-                new() {Confidence=0.9f,Label="Flower", Dimensions = new
-                    BoundingBoxDimensions() { Height = 1, Width = 1, Y = 1, X= 1} },
-            }} );
-        await Task.Delay(1000);
+        var tasks = new List<Task<ObjectDetectionResult>>();
+
+        foreach (var imageData in imagesSceneData) tasks.Add(Task.Run(() => DetectObjectsInImage(imageData)));
+
+        var results = await Task.WhenAll(tasks);
         return results;
     }
 
+    private ObjectDetectionResult DetectObjectsInImage(byte[] imageData)
+    {
+        var yolo = new Yolo();
+        var yoloOutput = yolo.Detect(imageData);
+
+        return new ObjectDetectionResult
+        {
+            ImageData = imageData,
+            Box = yoloOutput.Boxes
+        };
+    }
 }
+
+/*public async Task<IList<ObjectDetectionResult>> DetectObjectInScenesAsync(IList<byte[]>
+    imagesSceneData)
+{
+    IList<ObjectDetectionResult> results = new List<ObjectDetectionResult>();
+    results.Add(new ObjectDetectionResult
+    { ImageData = [0],
+        Box = new List<BoundingBox>()
+        {
+            new() {Confidence=0.5f,Label="Car", Dimensions = new
+                BoundingBoxDimensions() { Height = 2, Width = 2, Y = 0, X= 0} },
+        }} );
+    results.Add(new ObjectDetectionResult
+    { ImageData = [0],
+        Box = new List<BoundingBox>()
+        {
+            new() {Confidence=0.9f,Label="Flower", Dimensions = new
+                BoundingBoxDimensions() { Height = 1, Width = 1, Y = 1, X= 1} },
+        }} );
+    await Task.Delay(1000);
+    return results;
+}
+*/
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/Shinotobira/Anthony-Parent-ObjectDetection
